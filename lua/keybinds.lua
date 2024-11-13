2c34378da07a572a551a769fd8e64bcee9b21c80
Daanooo <73757140+Daanooo@users.noreply.github.com>
2024-11-13 22:44:57 +0100

    some docs for own reference

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,35 +1,46 @@
 local telescope = require('telescope.builtin')
 
--- [[ Window keybinds ]]
+--[[
+Window keybinds
 
+Prefix: t
+--]]
 vim.api.nvim_set_keymap('n', '<leader>tn', ':tabnew<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tt', ':tabnext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tp', ':tabprev<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tc', ':tabclose<CR>', { noremap = true, silent = true })
 
 
--- [[ Telescope keybinds ]]
+--[[
+Telescope keybinds
 
--- Finding stuff
+Prefix: f
+--]]
 vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>fc', telescope.commands, { desc = 'Telescope available commands' })
-vim.keymap.set('n', '<leader>kms', telescope.keymaps, { desc = 'Telescope show keymaps' })
-
--- Git
+vim.keymap.set('n', '<leader>fkb', telescope.keymaps, { desc = 'Telescope show keymaps' })
 vim.keymap.set('n', '<leader>gst', telescope.git_status, { desc = 'Telescope git status' })
 
 
--- [[ Sidebar keybinds ]]
+--[[
+Sidebar keybinds
+
+Prefix: e
+--]]
 vim.keymap.set('n', '<C-e>e', ':Neotree filesystem reveal left<CR>', { desc = 'Neotree focus', noremap = true, silent = true })
 vim.keymap.set('n', '<leader>ee', ':Neotree filesystem reveal left<CR>', { desc = 'Neotree focus', noremap = true, silent = true })
 vim.keymap.set('n', '<C-e>c', ':Neotree close<CR>', { desc = 'Neotree close', noremap = true, silent = true })
 vim.keymap.set('n', '<leader>ec', ':Neotree close<CR>', { desc = 'Neotree close', noremap = true, silent = true })
 
 
--- [[ Autocomplete keybinds ]]
+--[[
+Autocomplete keybinds
+
+Prefix: mixed I guess
+--]]
 -- These are in a function because they should only be set when file has an lsp available
 function set_cmp_keybinds(opts)
     vim.keymap.set('n', 'X', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
